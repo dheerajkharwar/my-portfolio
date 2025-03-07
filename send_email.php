cc71be7bbc379a06928c4ae6ee876c32956d5746
Dheeraj Kharwar <100955259+dheerajkharwar@users.noreply.github.com>
2025-03-07 09:13:30 +0530

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -7,7 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = htmlspecialchars(trim($_POST["message"]));
 
     // Set the recipient email address
-    $to = "dheerajkharwar102@gmail.com";  // Your email address
+    $to = "dheerajkharwar9@gmail.com";  // Your email address
     $headers = "From: " . $email . "\r\n";
     $headers .= "Reply-To: " . $email . "\r\n";
     $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
